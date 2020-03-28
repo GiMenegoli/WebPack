752d6b9d682e282c64d710ca773ef31c917a4e22
Giovanna Menegoli <52014893+GiMenegoli@users.noreply.github.com>
2020-03-28 12:46:24 -0300

    Aula 02

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,5 +1,11 @@
 //modulo nodejs
 const path = require('path');
+const babiliPlugin = require('babili-webpack-plugin');
+let plugins = [];
+
+if(process.env.NODE_ENV == 'production'){
+    plugins.push(new babiliPlugin);
+}
 
 module.exports = {
     entry: './app-src/app.js',
@@ -15,9 +21,10 @@ module.exports = {
                 use: {
                     loader: 'babel-loader'
                 }
-            }
+            },
         ]
 
-    }
+    },
+    plugins: plugins
 
 }
